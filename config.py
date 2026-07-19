4295f587a58347e5b107ebd6dca09a744edea1ae
dirksan28 <dirk-schmid@web.de>
2026-07-19 13:28:22 +0200

    Stop tracking config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Konfiguration für das Backup-Skript
-# Passe die Werte an oder lege sie als Umgebungsvariablen fest. test
-GITHUB_USERNAME = os.getenv("GITHUB_USERNAME", "YOUR_GITHUB_USERNAME")
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "YOUR_GITHUB_PERSONAL_ACCESS_TOKEN")
-BACKUP_DIR = os.getenv("BACKUP_DIR", "./my_gists_backup")
